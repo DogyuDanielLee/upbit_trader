bb6b0652dbbc56ba138bd5a14b76436cb377df43
DogyuLee <dgyulee@snu.ac.kr>
2023-01-25 03:01:40 +0900

    Telegram Base Structure Implemented

--- utils/telegram_api.py
+++ utils/telegram_api.py
@@ -1,3 +1,19 @@
 
-class TelegramBot:
+class TelegramAPI:
+    def __init__(self):
+        pass
+
+    def _set_token_(self):
+        pass
+
+    def sendPNG(self):
+        pass
+
+    def sendJPG(self):
+        pass
+    
+    def sendText(self):
+        pass
+
+class TelegramSmartBot:
     pass
